96b20d92264a5c7b23abc3ae0fc42db17e3dd3f4
gghez <gghez@PC9.infinegroup.local>
2012-10-22 14:53:50 +0200

    Add LengthValidator for length checking

---
+++ Stdlib/Validator/LengthValidator.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Accelerator\Stdlib\Validator;
+
+/**
+ * Description of LengthValidator
+ *
+ * @author gg00xiv
+ */
+class LengthValidator extends \Accelerator\Stdlib\Validator {
+
+    private $min;
+    private $max;
+
+    public function __construct($size) {
+        if (is_array($size)) {
+            if (count($size) != 2)
+                throw new \Accelerator\Exception\ArgumentException('$size', 'Must be an array of two values (ex: array(2,5))');
+            $this->min = $size[0];
+            $this->max = $size[1];
+        }else {
+            $this->min = 0;
+            $this->max = $size;
+        }
+    }
+
+    public function validate($input) {
+        return strlen($input) >= $this->min && strlen($input) <= $this->max;
+    }
+
+}
+
+?>
