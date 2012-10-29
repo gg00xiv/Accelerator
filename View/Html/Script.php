62fa9d98759044db7c95cd0b718e2bc0725ceda2
gghez <gghez@PC9.infinegroup.local>
2012-10-29 12:33:42 +0100

    mustCloseTag = true was missing

--- View/Html/Script.php
+++ View/Html/Script.php
@@ -9,9 +9,10 @@ namespace Accelerator\View\Html;
  */
 class Script extends HtmlElement {
 
+    protected $mustCloseTag = true;
+
     public function __construct($src = null, array $attributes = null) {
         parent::__construct('script', array_merge(array('type' => 'text/javascript'), $src ? array('src' => $src) : array(), $attributes? : array()));
-        $this->setInnerHtml('');
     }
 
 }
